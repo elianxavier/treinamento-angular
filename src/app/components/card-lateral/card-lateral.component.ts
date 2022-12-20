1337c55688cfee660e32475608b6215753c93d2e
Matheus Magno <matheusmagnon@gmail.com>
2022-12-20 18:15:14 -0300

    feat: add custom components

--- src/app/components/card-lateral/card-lateral.component.ts
+++ src/app/components/card-lateral/card-lateral.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-card-lateral',
-  templateUrl: './card-lateral.component.html',
-  styleUrls: ['./card-lateral.component.scss']
+  selector: "app-card-lateral",
+  templateUrl: "./card-lateral.component.html",
+  styleUrls: ["./card-lateral.component.scss"],
 })
 export class CardLateralComponent {
-
+  @Input() title: string;
+  @Input() image: string;
+  @Input() callToAction: string;
+  @Input() heightImage: string;
 }
