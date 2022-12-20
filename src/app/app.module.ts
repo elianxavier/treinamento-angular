7b1096a51d0488c24c68643f3b6a0bf61b43c7cf
Igor Santos <iggor.jsantos@gmail.com>
2022-12-19 22:47:51 -0300

    feat: inclusão do httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -22,7 +23,7 @@ import { CardLateralComponent } from './components/card-lateral/card-lateral.com
     CardPrincipalComponent,
     CardLateralComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
